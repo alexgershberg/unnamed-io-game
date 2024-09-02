e7db40bb82f98c31dbb5b9dcb40d7d83ac4fecf1
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2024-09-02 15:03:16 +0200

    Reminders & Fullscreen mode

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,25 @@
+function resizeCanvas(canvas: HTMLCanvasElement) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+function fullscreen() {
+    let canvas = <HTMLCanvasElement>document.getElementById("canvas");
+    let a = canvas.requestFullscreen();
+}
+
 let canvas = <HTMLCanvasElement>document.getElementById("canvas")!;
+resizeCanvas(canvas);
+window.addEventListener("resize", () => {
+    resizeCanvas(canvas);
+});
+
+window.addEventListener("keypress", (event) => {
+    if (event.key == "O") {
+        fullscreen();
+    }
+});
+
 let ctx = canvas.getContext("2d")!;
 ctx.fillStyle = "#000000";
 ctx.fillRect(0, 0, 150, 150);
