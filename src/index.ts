0b120e75785544d74b2ce82128009ff2df63423a
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2024-09-06 19:52:11 +0100

    Restructure for main function

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from "uuid";
 function resizeCanvas(canvas: HTMLCanvasElement) {
     canvas.width = window.innerWidth - 6; // 6px is total border width 3px left + 3px right
     canvas.height = window.innerHeight - 6;
+    drawElements(canvas);
 }
 
 function fullscreen() {
@@ -11,20 +12,27 @@ function fullscreen() {
     let a = canvas.requestFullscreen();
 }
 
-let canvas = <HTMLCanvasElement>document.getElementById("canvas")!;
-window.addEventListener("resize", () => {
+function drawElements(canvas: HTMLCanvasElement) {
+    let ctx = canvas.getContext("2d")!;
+    ctx.fillStyle = "#000000";
+    ctx.fillRect(0, 0, 150, 150);
+
+    console.log(uuidv4());
+}
+
+function main() {
+    let canvas = <HTMLCanvasElement>document.getElementById("canvas")!;
+    window.addEventListener("resize", () => {
+        resizeCanvas(canvas);
+    });
+
+    window.addEventListener("keypress", (event) => {
+        if (event.key === "O") {
+            fullscreen();
+        }
+    });
+
     resizeCanvas(canvas);
-});
-resizeCanvas(canvas);
+}
 
-window.addEventListener("keypress", (event) => {
-    if (event.key === "O") {
-        fullscreen();
-    }
-});
-
-let ctx = canvas.getContext("2d")!;
-ctx.fillStyle = "#000000";
-ctx.fillRect(0, 0, 150, 150);
-
-console.log(uuidv4());
+main();
